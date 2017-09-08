4b13cd20465799c3edcf8ee2f05b045ad6cc9ed4
Remi van Veen <remi.vanveen@anycode.nl>
2017-09-08 10:30:08 +0200

    Better solution for 8

--- remi/ex8.hs
+++ remi/ex8.hs
@@ -1,4 +1,5 @@
 -- Started at 13:15, stopped 15:30
+-- Evening: started at 19:45, finished at 20:15
 
 module Lab1 where
 import Data.List
@@ -9,27 +10,27 @@ data Boy = Matthew | Peter | Jack | Arnold | Carl
 
 boys = [Matthew, Peter, Jack, Arnold, Carl]
 
--- Determined on paper by writing down who accuses who (directly or indirectly)
+-- First I wrote out all cases by determing who (directly or indirectly)
+-- accuses who. But that way I was solving part of the programming problem
+-- on paper... Then realized we can just write out the boys' statements as
+-- boolean expressions. (Which is also a lot easier than working everything
+-- out on paper...)
+-- For Arnold, /= is used which functions as an exclusive OR, as
+-- Matthew or Peter is speaking the truth, but not both.
 accuses :: Boy -> Boy -> Bool
-accuses Matthew Peter = True
-accuses Matthew Jack = True
-accuses Matthew Arnold = True
-accuses Peter Matthew = True
-accuses Peter Jack = True
-accuses Jack Carl = True
-accuses Jack Arnold = True
-accuses Arnold Peter = True
-accuses Arnold Matthew = True
-accuses Carl Jack = True
-accuses a b = False
+accuses Matthew b = not (b == Carl) && not (b == Matthew)
+accuses Peter b = b == Matthew || b == Jack
+accuses Jack b = not (accuses Matthew b) && not (accuses Peter b)
+accuses Arnold b = accuses Matthew b /= accuses Peter b
+accuses Carl b = not (accuses Arnold b)
 
 accusers :: Boy -> [Boy]
 accusers t = [f | f <- boys, accuses f t]
 
--- jack
+-- Three people always speak the truth, so find who's accused by exactly three people.
+-- The other two always lie, so it's impossible that they accussed the guilty one.
 guilty :: [Boy]
 guilty = [b | b <- boys, length (accusers b) == 3]
 
--- matthew, peter, carl
 honest :: [Boy]
 honest = accusers (head guilty)
