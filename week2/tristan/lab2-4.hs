56f0d7a2c6d7bdc4e750a1ef94d7c5e2420d9227
Scoudem <tvvaalen@gmail.com>
2017-09-12 11:20:34 +0200

    Improve lab2-4

--- week2/tristan/lab2-4.hs
+++ week2/tristan/lab2-4.hs
@@ -1,9 +1,18 @@
--- 
+-- This took ~15 minutes
 
 import Data.List
 import Data.Char
 import System.Random
 import Test.QuickCheck
 
-isPermutation :: (Eq a, Ord a) => [a] -> [a] -> Bool
-isPermutation x y = sort (x) == sort (y)
+isPermutation :: Eq a => [a] -> [a] -> Bool
+isPermutation x y = isEqualLength x y && dropDuplicates x y == [] 
+ 
+dropDuplicates :: Eq a => [a] -> [a] -> [a]
+dropDuplicates x y = x \\ y
+
+isEqualLength :: Eq a => [a] -> [a] -> Bool
+isEqualLength x y = length x == length y
+
+main = do
+	print (isPermutation [1, 2, 3] [2, 5, 1])
