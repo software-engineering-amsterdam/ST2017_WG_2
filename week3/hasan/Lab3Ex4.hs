3fd3b7f11db7c9e90d91cf8999aaddc8994a4979
Hasan <H.Bilen@student.tudelft.nl>
2017-09-19 12:51:09 +0200

    Added testing for properties in ex4 (and replaced tabs with spaces).

--- week3/hasan/Lab3Ex4.hs
+++ week3/hasan/Lab3Ex4.hs
@@ -5,23 +5,28 @@ import System.Random
 import Test.QuickCheck
 import Lecture3
 import Lab3Ex1
+import Lab3Ex3
 import Control.Monad
 
 arbitraryForm :: Gen Form
 arbitraryForm  = do
-	depth <- choose (0, 10)
-	createForm depth
+    depth <- choose (0, 8)
+    createForm depth
 
 createForm :: Int -> Gen Form
 createForm 0 = liftM Prop (choose (0, 10))
 createForm depth = oneof [
-	liftM Neg (createForm depth2), 
-	liftM Dsj (vectorOf 2 (createForm depth2)), 
-	liftM Cnj (vectorOf 2 (createForm depth2)),
-	liftM2 Impl (createForm depth2) (createForm depth2),
-	liftM2 Equiv (createForm depth2) (createForm depth2)
-	] where depth2 = depth-1
+    liftM Neg (createForm depth2), 
+    liftM Dsj (vectorOf 2 (createForm depth2)), 
+    liftM Cnj (vectorOf 2 (createForm depth2)),
+    liftM2 Impl (createForm depth2) (createForm depth2),
+    liftM2 Equiv (createForm depth2) (createForm depth2)
+    ] where depth2 = depth-1
+    
+    
+checkCNFProperty :: Form -> Bool
+checkCNFProperty f = equiv f (cnf f)
+checkPropNames f =  all (\x -> elem x originalPropNames) (propNames (cnf f)) where originalPropNames = propNames f
 
 mainLab3Ex4 = do
-	form <- generate arbitraryForm
-	print (form)
+	quickCheck (forAll arbitraryForm (\f -> checkPropNames f && checkCNFProperty f))
