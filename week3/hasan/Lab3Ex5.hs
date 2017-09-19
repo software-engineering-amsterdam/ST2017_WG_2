35dfdbdc37ff835c506090b1eb5b2cd3dc70d752
Hasan <H.Bilen@student.tudelft.nl>
2017-09-19 15:32:19 +0200

    Added clause checker evaluator in ex5.

--- week3/hasan/Lab3Ex5.hs
+++ week3/hasan/Lab3Ex5.hs
@@ -13,6 +13,13 @@ cnf2cls (Neg x) = map (\y -> map (\z -> -z) y) (cnf2cls x)
 cnf2cls (Dsj x) = [concat (concat (map cnf2cls x))]
 cnf2cls (Cnj x) = concat (map cnf2cls x)
 
+evl2 :: Valuation -> Clauses -> Bool
+evl2 valuations clauses = all (\clause -> any (\clauseVariable -> if clauseVariable >= 0 then elem (clauseVariable, True) valuations else elem (-clauseVariable, False) valuations) clause) clauses
+
 mainLab3Ex5 = do
     let form = head (parse "*(8 4 (1<=>-5))")
-    print (cnf2cls (cnf form))
+    let valuations = [(8, True), (4, True), (5, False), (1, False)]
+    let clauses = [[8],[4],[-5,-1],[1,5]]
+    --let clauses = [[8], [4], [-5]]
+    --print (cnf2cls (cnf form))
+    print (evl2 valuations clauses)
