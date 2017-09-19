ba7cb8e471612569d3c3e3abb8b7414292d35472
V-Jong <vince-ny@hotmail.com>
2017-09-19 14:19:27 +0200

    Ex 5 fix

--- week3/vincent/ex5.hs
+++ week3/vincent/ex5.hs
@@ -1,6 +1,6 @@
 -- ID: 11408227
 -- Name: Vincent Jong
--- Time: 13:00 -  (Programming), (Testing), (Answering)
+-- Time: 13:00 - 14:10 (Programming), (Testing), (Answering)
 
 module Ex5 where
 
@@ -24,6 +24,7 @@ testForm5 = Cnj [p, Dsj [q, r], Dsj [q, s]]
 form2Clause :: Form -> Clause
 form2Clause (Prop x) = [x]
 form2Clause (Neg (Prop x)) = [-x]
+form2Clause (Dsj list) = forms2Clause list
 
 forms2Clause :: [Form] -> Clause
 forms2Clause [] = []
@@ -34,3 +35,10 @@ cnf2cls (Prop x) = [(form2Clause (Prop x))]
 cnf2cls (Neg (Prop x)) = [(form2Clause (Neg (Prop x)))]
 cnf2cls (Dsj list) = [forms2Clause list]
 cnf2cls (Cnj list) = map (\x -> form2Clause x) list
+
+main = do
+    print $ cnf2cls testForm1
+    print $ cnf2cls testForm2
+    print $ cnf2cls testForm3
+    print $ cnf2cls testForm4
+    print $ cnf2cls testForm5
