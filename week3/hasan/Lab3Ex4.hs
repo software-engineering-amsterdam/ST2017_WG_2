680e5d6daeb4ef34aedeafe16b6957b409c46dd8
Hasan <H.Bilen@student.tudelft.nl>
2017-09-19 12:15:07 +0200

    Simplified ex4 with liftM.

--- week3/hasan/Lab3Ex4.hs
+++ week3/hasan/Lab3Ex4.hs
@@ -5,51 +5,22 @@ import System.Random
 import Test.QuickCheck
 import Lecture3
 import Lab3Ex1
+import Control.Monad
 
 arbitraryForm :: Gen Form
 arbitraryForm  = do
 	depth <- choose (0, 10)
 	createForm depth
 
-	
-arbitaryProp :: Gen Form
-arbitaryProp = do
-	a <- choose (1,10) 
-	return (Prop a)
-	
-arbitaryNeg :: Int -> Gen Form
-arbitaryNeg depth = do
-	a <- createForm (depth-1)
-	return (Neg a)
-	
-arbitaryDsj :: Int -> Gen Form
-arbitaryDsj depth = do
-	n <- choose (2, 4)
-	x <- vectorOf n (createForm (depth-1))
-	return (Dsj x)
-	
-arbitaryCnj :: Int -> Gen Form
-arbitaryCnj depth = do
-	n <- choose (2, 4)
-	x <- vectorOf n (createForm (depth-1))
-	return (Cnj x)
-
-arbitaryImpl :: Int -> Gen Form
-arbitaryImpl depth = do
-	x <- createForm (depth-1)
-	y <- createForm (depth-1)
-	return (Impl x y)
-
-arbitaryEquiv :: Int -> Gen Form
-arbitaryEquiv depth = do
-	x <- createForm (depth-1)
-	y <- createForm (depth-1)
-	return (Equiv x y)
-
 createForm :: Int -> Gen Form
-createForm 0 = arbitaryProp
-createForm depth = oneof [arbitaryProp, (arbitaryNeg depth), (arbitaryDsj depth), (arbitaryCnj depth), (arbitaryImpl depth), (arbitaryEquiv depth)]
-
+createForm 0 = liftM Prop (choose (0, 10))
+createForm depth = oneof [
+	liftM Neg (createForm depth2), 
+	liftM Dsj (vectorOf 2 (createForm depth2)), 
+	liftM Cnj (vectorOf 2 (createForm depth2)),
+	liftM2 Impl (createForm depth2) (createForm depth2),
+	liftM2 Equiv (createForm depth2) (createForm depth2)
+	] where depth2 = depth-1
 
 mainLab3Ex4 = do
 	form <- generate arbitraryForm
