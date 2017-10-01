9a1bb84099323a1ffe762bf791db1a5bacb319ba
V-Jong <vince-ny@hotmail.com>
2017-10-01 17:12:54 +0200

    Final ex 1 and 2

---
+++ week4/final/ex1.hs
@@ -0,0 +1,5 @@
+module Ex1 where
+
+-- Question 1: Why does ordering matter if we have tuple (a, b) and a 2 element list [a, b] (page 133). 
+-- Questiom 2: Do infinite lists that pass their bounds keep on generating? (page 118)
+-- Question 3: example 4.25: what exactly is happening here? (page 134)
