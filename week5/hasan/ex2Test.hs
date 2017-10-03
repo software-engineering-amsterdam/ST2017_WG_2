2e3a34ed8a95f23b952aadd6e196eaf04ac099ad
Hasan <H.Bilen@student.tudelft.nl>
2017-10-03 11:06:12 +0200

    Finished ex2.

--- week5/hasan/ex2Test.hs
+++ week5/hasan/ex2Test.hs
@@ -1,33 +1,42 @@
+-- TEST REPORT
+-- Shoutout to Vincent for discovering the command ":set +s"
+--
+-- *Ex2Test> :set +s
+-- *Ex2Test> runWithEx1Test
+-- Ex1:
+--         All consistent:
+--         True
+-- (0.30 secs, 12,887,936 bytes)
+-- *Ex2Test> runWithEx2Test
+-- Ex2:
+--         All consistent:
+--         True
+-- (0.03 secs, 12,868,528 bytes)
+-- *Ex2Test>
+
+-- Above shows the correctness of both solutions as well the effiency with memory and speed
+
 module Ex2Test where
 
 import Ex1
 import Ex2
 import Lecture5
 import Test.QuickCheck
-import Data.Time.Clock
-import Data.Time.Clock.POSIX
 
 consistentHelper :: [Lecture5.Node] -> Bool
 consistentHelper [] = True
 consistentHelper ((s, xs):aa) = Lecture5.consistent s && consistentHelper aa
 
-runEx2Test = do
-    let sudokus = [Lecture5.example1, Lecture5.example2, Lecture5.example3, Lecture5.example4, Lecture5.example5]
-    
-    putStrLn "Ex1:"
-    startTime1 <- getPOSIXTime
-    let allConsistentEx1 = all (\sud -> consistentHelper (Lecture5.solveNs (Lecture5.initNode sud))) sudokus
-    putStr "\tAll consistent:\n\t"
-    print allConsistentEx1
-    endTime1 <- getPOSIXTime
-    putStr "\tTime taken:\n\t"
-    print (endTime1 - startTime1)
-    
+sudokus = [Lecture5.example1, Lecture5.example2, Lecture5.example3, Lecture5.example4]
+
+runWithEx2Test = do
     putStrLn "Ex2:"
-    startTime2 <- getPOSIXTime
-    let allConsistentEx2 = all (\sud -> consistentHelper (Lecture5.solveNs (Lecture5.initNode sud))) sudokus
+    let allConsistent = all (\sud -> consistentHelper (Lecture5.solveNs (Lecture5.initNode sud))) sudokus
     putStr "\tAll consistent:\n\t"
-    print allConsistentEx2
-    endTime2 <- getPOSIXTime
-    putStr "\tTime taken:\n\t"
-    print (endTime2 - startTime2)
+    print allConsistent
+    
+runWithEx1Test = do
+    putStrLn "Ex1:"
+    let allConsistent = all (\sud -> consistentHelper (Lecture5.solveNs (Lecture5.initNode sud))) sudokus
+    putStr "\tAll consistent:\n\t"
+    print allConsistent
